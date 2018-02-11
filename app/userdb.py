e6dc36822273676232ea4d3599945fd577cc1f76
Sudarshan Khasnis <sudarshan.khasnis@gmail.com>
2018-02-12 03:18:50 +0530

    UI Refined

--- app/userdb.py
+++ app/userdb.py
@@ -2,6 +2,6 @@ import sqlite3
 conn=sqlite3.connect('database.db')
 conn.execute('create table users (userid integer primary key autoincrement,first_name text, last_name text, email text,password text);')
 conn.commit()
-conn.execute('create table reviews(revid integer primary key autoincrement,movietitle text, userid integer, comments text,ratings real);')
+conn.execute('create table reviews(revid integer primary key autoincrement,movieid integer, userid integer, comments text,ratings real);')
 conn.commit()
 conn.close()
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, flash, redirect,session
 from app import app
 import sqlite3
+from random import randint
+
 @app.route('/')
 def index():
     return render_template("start.html")
@@ -8,11 +10,68 @@ def index():
 @app.route('/home',  methods =['GET', 'POST'])
 def home():
 	if request.method == 'GET':
-		con = sqlite3.connect("Movie.db")
-		cur = con.cursor()
-		rows=cur.execute("select * from Movies limit 0, 10")
-		return render_template("home.html", rows=rows)
+		if 'userid' in session:
+			con = sqlite3.connect("Movie.db")
+			cur = con.cursor()
+			rows=cur.execute("select * from Movies limit 0, 10")
+			return render_template("home.html", rows=rows)
+		else:
+			return redirect("http://localhost:5000/login")
 
+@app.route('/movie/<movie_id>',  methods =['GET', 'POST'])
+def movie(movie_id):
+	if request.method == 'GET':
+		if 'userid' in session:
+			con = sqlite3.connect("Movie.db")
+			cur = con.cursor()
+			rows=cur.execute("select * from Movies where Id=%s;" % movie_id)
+			con1=sqlite3.connect("database.db")
+			cur1 = con1.cursor()
+			cur2=con.cursor()
+			recom=cur2.execute("select * from Movies order by 'Movie Title' limit %d, %d" % (randint(5,15),randint(30,50)))
+			reviews=cur1.execute("select * from reviews where movieid=%s;" % movie_id)
+			return render_template("movie.html",rows=rows,reviews=reviews,recom=recom,reviewform=reviewform())
+		else:
+			return redirect("http://localhost:5000/login")
+
+	elif request.method == 'POST':
+		if 'userid' in session:
+			addreview(request.form['comments'],request.form['rate'],movie_id)
+			con = sqlite3.connect("Movie.db")
+			cur = con.cursor()
+			rows=cur.execute("select * from Movies where Id=%s;" % movie_id)
+			cur2=con.cursor()
+			recom=cur2.execute("select * from Movies limit %d, %d" % (randint(10,15),randint(20,30)))
+			con1=sqlite3.connect("database.db")
+			cur1 = con1.cursor()
+			reviews=cur1.execute("select * from reviews where movieid=%s;" % movie_id)
+			return render_template("movie.html",rows=rows,reviews=reviews,recom=recom,reviewform=reviewform())
+		else:
+			return redirect("http://localhost:5000/login")
+
+
+
+
+@app.route('/logout',  methods =['GET', 'POST'])
+def logout():
+	session.pop('userid',None)
+	return render_template("start.html")
+
+@app.route('/search',  methods =['GET', 'POST'])
+def search():
+	if request.method == 'POST':
+		if 'userid' in session:
+			con = sqlite3.connect("Movie.db")
+			cur = con.cursor()
+			if request.form['year']!="":
+				rows=cur.execute("select * from Movies where [Release Date]= %s;" % request.form['year'])
+			else:
+				rows=cur.execute("select * from Movies where [Movie Title] like '%s';" % request.form['name'])
+			return render_template("search.html",searchform=searchform(),rows=rows)
+		else:
+			return redirect("http://localhost:5000/login")
+	else:
+		return render_template("search.html",searchform=searchform())
 
 @app.route('/profile',  methods =['GET', 'POST'])
 def profile():
@@ -21,8 +80,10 @@ def profile():
 			print ("\n\n\n\n\n\n",session['userid'])
 			user_id = session['userid']
 			con = sqlite3.connect("database.db")
+			con1 = sqlite3.connect("database.db")
 			cur = con.cursor()
-			rows=cur.execute("select * from reviews where userid='%s';" % user_id)
+			cur1 = con1.cursor()
+			rows=cur1.execute("select * from reviews where userid='%s';" % user_id)
 			userdetails=cur.execute("select * from users where userid='%s';" % user_id)
 			return render_template("profile.html", rows=rows, userdetails=userdetails)
 		else:
@@ -47,7 +108,6 @@ def login():
 					for r in cur:
 						session['userid']= r[0]
 						break
-					print ("\n\n\n\n\n\n",session['userid'])
 					return redirect("http://localhost:5000/home")
 				else:
 					flash("Invalid Input")
@@ -77,6 +137,13 @@ def register(fn,ln,en,pd):
 	conn.close()
 	return
 
+def addreview(cm,rt,mi):
+	conn=sqlite3.connect('database.db')
+	conn.execute("insert into reviews (movieid,userid,comments,ratings) values('%s','%s','%s','%s');" % (mi,session['userid'],cm,rt))
+	conn.commit()
+	conn.close()
+	return
+
 def check(en,pd):
 	conn=sqlite3.connect('database.db')
 	cur=conn.execute("select email,password from users;")
@@ -95,6 +162,19 @@ import re
 from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField, PasswordField
 from wtforms import validators, ValidationError
 
+class reviewform(Form):
+	comments = TextField("Comments: ")
+	rate = RadioField('Rate: ', choices = [(2,'1'),(4,'2'),(6,'3'),(8,'4'),(10,'5')])
+	def validate(self):
+		return (True)
+
+
+class searchform(Form):
+	name = TextField("Movie title: ")
+	year = TextField("Year: ")
+	def validate(self):
+		return (True)
+
 class loginform(Form):
 	eid = TextField("Email Address*")
 	pwd = PasswordField("Password*")
@@ -110,7 +190,6 @@ class loginform(Form):
 		if len(self.pwd.data)<5:
 			valpass=False
 		return (match & valpass)
-
 
 class registerform(Form):
 	firstname = TextField("First Name*")
